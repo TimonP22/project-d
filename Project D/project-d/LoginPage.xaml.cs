14180f8a72a2af89009cd0c9ff5c2cbf01071ea1
Thomas <thomasriemslagbaas@gmail.com>
2024-05-15 11:04:22 +0200

    Bezig met de login werkend krijgen met specifiek email en wachtwoord

--- Project D/project-d/LoginPage.xaml.cs
+++ Project D/project-d/LoginPage.xaml.cs
@@ -2,10 +2,25 @@ namespace project_d;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    public async void OnLoginBtnClicked()
+    {
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+
+        if (email == "psycholoog@gmail.com" || password == "psycholoog123")
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+            await DisplayAlert("Login Failed", "Invalid email or password", "OK");
+        }
+    }
 
     private async void OnReturnBtnClicked(object sender, EventArgs e)
     {
