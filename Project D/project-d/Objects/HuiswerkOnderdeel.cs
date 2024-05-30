7cef3885c0d015e0bc0ca3c87513f4b32a7b7701
Timon <timonp22@gmail.com>
2024-05-30 15:23:03 +0200

    Create HuiswerkOnderdeel.cs

---
+++ Project D/project-d/Objects/HuiswerkOnderdeel.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace project_d.Objects;
+
+abstract class HuiswerkOnderdeel
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public int HomeworkId { get; set; }
+    public string? Text { get; set; }
+
+    protected HuiswerkOnderdeel(int homeworkId, string text)
+    {
+        HomeworkId = homeworkId;
+        Text = text;
+    }
+}
