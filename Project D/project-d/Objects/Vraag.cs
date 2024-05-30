55f2c6e7fd7690d2999524939bc2b065815c1ed5
Thomas <thomasriemslagbaas@gmail.com>
2024-05-30 16:32:21 +0200

    Create Vraag.cs

---
+++ Project D/project-d/Objects/Vraag.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace project_d.Objects
+{
+    [Table("Vragen")]
+    public class Vraag : HuiswerkOnderdeel
+    {
+        public Vraag(int homeworkId, string text)
+            : base(homeworkId, text)
+        {
+        }
+
+        public Vraag()
+        { }
+    }
+}
